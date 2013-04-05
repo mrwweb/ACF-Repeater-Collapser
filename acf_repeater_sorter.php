ca6c5d33317982d55a38e8eb15aba71790f8621d
Mark Root-Wiley <root.wiley+git@gmail.com>
2013-04-05 12:05:43 -0700

    new hook for 4.0 compatibility
    thanks @brasofilo!

--- acf_repeater_sorter.php
+++ acf_repeater_sorter.php
@@ -5,18 +5,15 @@
  * Version 0.2
  */
 
-add_action( 'acf_print_scripts-input', 'acf_repeater_sorter_scripts' );
+/* Load the javascript and CSS files on the ACF admin pages */
+add_action( 'acf/input/admin_enqueue_scripts', 'acf_repeater_sorter_assets' );
 add_action( 'acf_print_styles-input', 'acf_repeater_sorter_styles' ); 
-
-function acf_repeater_sorter_scripts() {
+function acf_repeater_sorter_assets() {
 	wp_enqueue_script(
 		'acf_repeater_sorter_admin_js',
 		esc_url( plugins_url( 'js/acf_repeater_sorter_admin.js', __FILE__ ) ),
 		array( 'jquery' )
 	);
-}
-
-function acf_repeater_sorter_styles() {
 	wp_enqueue_style(
 		'acf_repeater_sorter_admin_css',
 		esc_url( plugins_url( 'css/acf_repeater_sorter_admin.css', __FILE__ ) )
