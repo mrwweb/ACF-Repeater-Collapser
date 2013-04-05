9b82b391775808d035304986c13b781005a190ab
Mark Root-Wiley <root.wiley+git@gmail.com>
2013-04-05 14:41:05 -0700

    add commenting to js

--- js/acf_repeater_sorter_admin.js
+++ js/acf_repeater_sorter_admin.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function($) {
 
+	// toggle the class that collapses the repeater
+	// toggle appropriate button text
 	function acf_repeater_toggle( event ) {
 	    // Toggle Fields
 	    $(this).parent().toggleClass('collapsed-repeater');
@@ -12,16 +14,17 @@ jQuery(document).ready(function($) {
 	    }
 	}
 
+	// HTML to put above each repeater instance
 	$collapseButton = '<input class="button field-repeater-toggle" type="button" value="Collapse Fields" />';
 
+	// find each repeater instance, add the button if the field uses the row layout
 	$('.field_type-repeater').each( function() {
-
 		if( $( '.acf-input-table', $(this) ).hasClass('row_layout') ) {
 			$(this).prepend( $collapseButton );
 		}
-
 	});
 
+	// bind the click event to the toggle function
 	$( '.field-repeater-toggle' ).on(
 		'click',
 		acf_repeater_toggle
