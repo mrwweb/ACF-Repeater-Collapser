25c2ac2b19b42342d2caa3e1832607ef6f55dd3a
Mark Root-Wiley <root.wiley+git@gmail.com>
2013-04-09 06:29:38 -0700

    fix for issue #3
    correct filesnames

--- acf_repeater_collapser.php
+++ acf_repeater_collapser.php
@@ -3,21 +3,21 @@
  * Plugin Name: Advanced Custom Fields Repeater Collapser
  * Plugin URI: https://github.com/mrwweb/ACF-Repeater-Collapser
  * Description: Provides a way to collapse and expand repeater field instances in order to enable better sorting.
- * Version 1.0.0
+ * Version 1.0.1
  * Author: Mark Root-Wiley
  * Author URI: http://mrwweb.com
  */
 
 /* Load the javascript and CSS files on the ACF admin pages */
-add_action( 'acf/input/admin_enqueue_scripts', 'acf_repeater_sorter_assets' );
-function acf_repeater_sorter_assets() {
+add_action( 'acf/input/admin_enqueue_scripts', 'acf_repeater_collapser_assets' );
+function acf_repeater_collapser_assets() {
 	wp_enqueue_script(
-		'acf_repeater_sorter_admin_js',
-		esc_url( plugins_url( 'js/acf_repeater_sorter_admin.js', __FILE__ ) ),
+		'acf_repeater_collapser_admin_js',
+		esc_url( plugins_url( 'js/acf_repeater_collapser_admin.js', __FILE__ ) ),
 		array( 'jquery' )
 	);
 	wp_enqueue_style(
-		'acf_repeater_sorter_admin_css',
-		esc_url( plugins_url( 'css/acf_repeater_sorter_admin.css', __FILE__ ) )
+		'acf_repeater_collapser_admin_css',
+		esc_url( plugins_url( 'css/acf_repeater_collapser_admin.css', __FILE__ ) )
 	);
 }
