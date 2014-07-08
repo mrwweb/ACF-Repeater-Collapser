4f06d5942e95978d5f14cbb8674db973fb77a136
mrwweb <info@mrwweb.com>
2014-07-08 15:39:25 -0700

    check-in functioning prototype, flexible fields and nested repeaters now broken

--- js/acf_repeater_collapser_admin.js
+++ js/acf_repeater_collapser_admin.js
@@ -2,33 +2,36 @@ jQuery(document).ready(function($) {
 
 	// toggle the class that collapses the repeater
 	// toggle appropriate button text
-	function acfRepeaterCollapseAll( event ) {
+	function acfRepeaterToggleAll() {
 		$rowsetButton = $(this);
-		$rowsetWrapper = $(this).parent();
-
-	    // Toggle Fields
-	    $rowsetWrapper.toggleClass('collapsed-repeater');
+		$rowsetWrapper = $(this).parents('.field');
+		$row = $('.row', $rowsetWrapper);
 	    
+	    // Change Row States & Add Button Text
 	    // Nice Button Text
-	    if( $rowsetWrapper.hasClass('collapsed-repeater') ) {
+	    if( false === $rowsetWrapper.data('acf-rows-collapsed') ) {
+	    	$row.addClass('collapsed-row').data('acf-row-collapsed',true);
+	    	$rowsetWrapper.addClass('collapsed-repeater').data('acf-rows-collapsed',true);
 	    	$rowsetButton.text('Expand All Rows');
 	    } else {
+	    	$row.removeClass('collapsed-row').data('acf-row-collapsed',false);
+	    	$rowsetWrapper.removeClass('collapsed-repeater').data('acf-rows-collapsed',false);
 	    	$rowsetButton.text('Collapse All Rows');
 	    }
 	}
 
-	function acfRepeaterCollapseSingle( event ) {
+	function acfRepeaterToggleSingle() {
 		$rowButton = $(this);
-		$row = $rowButton.parent().parent();
-
-	    // Toggle Fields
-	    $row.toggleClass('collapsed-row');
+		$row = $rowButton.parents('.row');
+		$rowButtonText = $('.screen-reader-text', $rowButton);
 	    
 	    // Nice Button Text
-	    if( $row.hasClass('collapsed-row') ) {
-	    	$('.screen-reader-text', $rowButton).text('Expand Row');
+	    if( false === $row.data('acf-row-collapsed') ) {
+	    	$row.addClass('collapsed-row').data('acf-row-collapsed',true);
+	    	$rowButtonText.text('Expand Row')
 	    } else {
-	    	$('.screen-reader-text', $rowButton).text('Collapse Row');
+	    	$row.removeClass('collapsed-row').data('acf-row-collapsed',false);
+	    	$rowButtonText.text('Collapse Row')
 	    }
 	}
 
@@ -40,17 +43,20 @@ jQuery(document).ready(function($) {
 	$('.field_type-repeater, .field_type-flexible_content').each( function() {
 		$repeater = $(this);
 		if( $( '.acf-input-table', $repeater ).hasClass('row_layout') ) {
+			$repeater.data('acf-rows-collapsed', false);
 			if( $repeater.is( 'tr' ) ) {
 				$repeater.children( 'td:last-child' ).children( '.inner' ).prepend( $collapseAllButton );
+				$('.row', $repeater ).data('acf-row-collapsed',false);
 			} else {
 				$repeater.prepend( $collapseAllButton );
+				$('.row', $repeater ).data('acf-row-collapsed',false);
 			}
 		}
 	});
 
 	// append single repeater collapse to each row of repeater field
 	$('.field_type-repeater .row_layout .row').each( function() {
-		$(this).prepend( $collapseSingleButton );
+		$(this).prepend( $collapseSingleButton ).data('acf-row-collapsed',false);
 	});
 
 	// bind the click event to the toggle functions
@@ -59,12 +65,12 @@ jQuery(document).ready(function($) {
 	$( 'div.field_type-repeater, div.field_type-flexible_content' ).on(
 		'click',
 		'.field-repeater-toggle-all',
-		acfRepeaterCollapseAll
+		acfRepeaterToggleAll
 	);
 	$( '.field_type-repeater .row_layout .row' ).on(
 		'click',
 		'.field-repeater-toggle-single',
-		acfRepeaterCollapseSingle
+		acfRepeaterToggleSingle
 	);
 
 });
