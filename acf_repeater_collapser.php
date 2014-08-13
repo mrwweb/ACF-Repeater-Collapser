a2187f47be973f4e731a47f22c5f17a7bf09d674
mrwweb <info@mrwweb.com>
2014-08-13 15:07:03 -0700

    add plugin versioning for styles and scripts along with acf version check for 4 AND 5 support.

--- acf_repeater_collapser.php
+++ acf_repeater_collapser.php
@@ -8,22 +8,40 @@
  * Author URI: http://mrwweb.com
  */
 
+define( 'ACF_REPEATER_COLLAPSER_VERSION', '1.3.0' );
+
 /* Load the javascript and CSS files on the ACF admin pages */
 // 11 helps take precedence over core styles
 add_action( 'acf/input/admin_enqueue_scripts', 'acf_repeater_collapser_assets', 11 );
 function acf_repeater_collapser_assets() {
-	if( ! function_exists( 'acf_get_setting' ) ) {
+	$acf_version = acf()->settings['version'];
+	if( version_compare( $acf_version, '4.0', '>=' ) && version_compare( $acf_version, '5.0', '<' ) ) {
 		// version 4.X
 		wp_enqueue_script(
 			'acf_repeater_collapser_admin_js',
-			esc_url( plugins_url( 'js/acf_repeater_collapser_admin.js', __FILE__ ) ),
-			array( 'jquery' )
+			esc_url( plugins_url( 'js/acf4_repeater_collapser_admin.js', __FILE__ ) ),
+			array( 'jquery' ),
+			ACF_REPEATER_COLLAPSER_VERSION
 		);
 		wp_enqueue_style(
 			'acf_repeater_collapser_admin_css',
-			esc_url( plugins_url( 'css/acf_repeater_collapser_admin.css', __FILE__ ) )
+			esc_url( plugins_url( 'css/acf4_repeater_collapser_admin.css', __FILE__ ) ),
+			false,
+			ACF_REPEATER_COLLAPSER_VERSION
 		);
-	} else {
+	} elseif( version_compare( $acf_version, '5.0', '>=' ) ) {
 		// version 5.X
+		wp_enqueue_script(
+			'acf_repeater_collapser_admin_js',
+			esc_url( plugins_url( 'js/acf5_repeater_collapser_admin.js', __FILE__ ) ),
+			array( 'jquery' ),
+			ACF_REPEATER_COLLAPSER_VERSION
+		);
+		wp_enqueue_style(
+			'acf_repeater_collapser_admin_css',
+			esc_url( plugins_url( 'css/acf5_repeater_collapser_admin.css', __FILE__ ) ),
+			false,
+			ACF_REPEATER_COLLAPSER_VERSION
+		);
 	}
 }
